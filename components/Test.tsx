986471d33e5d98bbf849028573cc3fcc2e1f36c9
godcl1623 <godcl1623@gmail.com>
2022-09-07 21:20:15 +0900

    컴포넌트 생성 테스트

---
+++ components/Test.tsx
@@ -0,0 +1,5 @@
+export default function Test() {
+  return (
+    <h1>Index Page Test</h1>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        html,
+        body {
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+
+        * {
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        @media (prefers-color-scheme: dark) {
+          html {
+            color-scheme: dark;
+          }
+          body {
+            color: white;
+            background: black;
+          }
+        }
+      `}</style>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
