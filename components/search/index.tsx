4d316b5890397ee186d4fb0414ee3dbd3b1828ce
godcl1623 <godcl1623@gmail.com>
2022-09-09 22:31:14 +0900

    #1 feat : 검색 페이지 이동 로직 기본형 구현

--- components/search/index.tsx
+++ components/search/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import SelectBox from './SelectBox';
+import { useRouter } from 'next/router';
 
 export default function Search() {
+  const router = useRouter();
+
   function submitHandler(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log(event);
+    const searchEngineSelectBox = event.currentTarget[0];
+    const searchInput = event.currentTarget[1];
+    let selectedSearchEngine = '';
+    let inputValue = '';
+    if (searchEngineSelectBox instanceof HTMLSelectElement) selectedSearchEngine = searchEngineSelectBox.value;
+    if (searchInput instanceof HTMLInputElement) inputValue = searchInput.value;
+    window.open(`https://www.google.com/search?q=${inputValue}`, '_newtab');
   }
+
   return (
     <form className='flex-center w-full h-12 shadow-lg dark:shadow-zinc-600' onSubmit={submitHandler}>
       <SelectBox />
@@ -19,7 +29,7 @@ export default function Search() {
         name='submit'
         type='submit'
         value='검색'
-        className='w-24 h-full rounded-r-md bg-sky-400 text-white dark:bg-sky-800 dark:text-gray-300'
+        className='w-24 h-full rounded-r-md bg-sky-400 text-white dark:bg-sky-800 dark:text-gray-300 cursor-pointer'
       />
     </form>
   );
