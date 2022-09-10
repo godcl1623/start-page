7e1f21ca7a67583828306d1e68eda3997c60fb72
godcl1623 <godcl1623@gmail.com>
2022-09-10 17:20:35 +0900

    #3 test : RSS 피드 연결 테스트 성공

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/rss',
+        destination: 'https://fe-developers.kakaoent.com/rss.xml',
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
