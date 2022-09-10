c450ace1f6091e19e821310a5dbfaa96bbd5f0ee
godcl1623 <godcl1623@gmail.com>
2022-09-10 18:04:02 +0900

    #5 refactor : getServerSideProps를 올바른 사용법으로 수정, xml 파싱 테스트 진행 및 성공

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
+import axios from 'axios';
 import Search from 'components/search';
+import React from 'react';
 
-export default function Index() {
+interface IndexProps {
+  rssResponse: string;
+}
+
+export default function Index({ rssResponse }: IndexProps) {
+  React.useEffect(() => {
+    if (rssResponse) {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(rssResponse, 'text/xml');
+      console.log(xml);
+    }
+  }, [rssResponse]);
+
   return (
     <article className='flex-center w-full h-full bg-neutral-100 dark:bg-neutral-800 dark:text-white'>
       <section className='flex-center w-1/2 h-full'>
@@ -9,3 +23,16 @@ export default function Index() {
     </article>
   );
 }
+
+export async function getServerSideProps() {
+  try {
+    const { data } = await axios.get('http://localhost:3000/rss');
+    return {
+      props: {
+        rssResponse: data
+      }
+    };
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message);
+  }
+};
