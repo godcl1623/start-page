2a40a1c5f26657dca15c41e48f8a92b34f7c14f4
godcl1623 <godcl1623@gmail.com>
2022-11-27 02:31:19 +0900

    #17 feature: urls api 대체용 sources api 신설

---
+++ controllers/sources/index.ts
@@ -0,0 +1,78 @@
+import { promises as fs } from "fs";
+
+export interface SourceDataInput {
+    name: string;
+    url: string;
+}
+
+export interface SourceData extends SourceDataInput {
+    id: number;
+}
+
+export type SourceDataToModify = Partial<SourceDataInput>;
+
+export interface FileContentsInterface {
+    sources: SourceData[];
+}
+
+export class CustomError {
+    code: number;
+    message: string;
+    constructor(code: number, message: string) {
+        this.code = code;
+        this.message = message;
+    }
+}
+
+export const checkIfDataValid = (
+    dataToCheck: unknown,
+    valuesCount: number,
+    typeStandard: string
+) => {
+    if (dataToCheck && typeof dataToCheck === "object") {
+        if (
+            Object.values(dataToCheck).length < valuesCount ||
+            Object.values(dataToCheck).some(
+                (objectValue: unknown) =>
+                    typeof objectValue !== typeStandard || objectValue === ""
+            )
+        ) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+};
+
+export const checkIfDataExists = (
+    storedList: unknown[],
+    dataToCheck: unknown
+) => {
+    if (storedList.includes(dataToCheck)) return true;
+    return false;
+};
+
+export const updateData = <T extends object>(
+    storedList: unknown[],
+    jsonDirectory: string,
+    dataToAdd: T
+) => {
+    const sourceId = storedList.length;
+    const newSource = {
+        id: sourceId,
+        ...dataToAdd,
+    };
+    const updatedSources = [...storedList, newSource];
+    const body = {
+        sources: updatedSources,
+    };
+    try {
+        fs.writeFile(`${jsonDirectory}/sources.json`, JSON.stringify(body));
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
