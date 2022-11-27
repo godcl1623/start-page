102ad478e18dc0b367b4d7e70e3e0120f268c208
godcl1623 <godcl1623@gmail.com>
2022-11-28 00:11:13 +0900

    #17 feature: 피드 관리용 api 생성

---
+++ pages/api/feeds/[feedsSetId]/[feedId].ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { promises as fs } from "fs";
+import { areEqual } from "common/capsuledConditions";
+import { JSON_DIRECTORY } from 'common/constants';
+
+export default async function feedsSetIdHandler(
+    request: NextApiRequest,
+    response: NextApiResponse
+) {
+    const fileContents = await fs.readFile(
+        `${JSON_DIRECTORY}/feeds.json`,
+        "utf8"
+    );
+    if (fileContents == null) {
+        response.status(404).send("file not exists.");
+    }
+    if (areEqual(request.method, "GET")) {
+        response.status(405).send("Method Not Allowed");
+    } else if (areEqual(request.method, "POST")) {
+        response.status(405).send("Method Not Allowed");
+    } else if (areEqual(request.method, "PUT")) {
+        response.status(405).send("Method Not Allowed");
+    } else if (areEqual(request.method, "PATCH")) {
+        response.status(200).send("success");
+    } else if (areEqual(request.method, "DELETE")) {
+        response.status(405).send("Method Not Allowed");
+    }
+}
