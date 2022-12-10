bee0ed8f4d87601587233f458bd72ff8fbbe779b
godcl1623 <godcl1623@gmail.com>
2022-12-11 00:55:09 +0900

    #17 feature: 개별 피드 PATCH api 작성

--- pages/api/feeds/[feedsSetId]/[feedId].ts
+++ pages/api/feeds/[feedsSetId]/[feedId].ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { promises as fs } from "fs";
 import { areEqual } from "common/capsuledConditions";
-import { JSON_DIRECTORY } from 'common/constants';
+import { JSON_DIRECTORY } from "common/constants";
+import { ParsedFeedsDataType, ParseResultType } from "types/global";
 
 export default async function feedsSetIdHandler(
     request: NextApiRequest,
@@ -21,7 +22,32 @@ export default async function feedsSetIdHandler(
     } else if (areEqual(request.method, "PUT")) {
         response.status(405).send("Method Not Allowed");
     } else if (areEqual(request.method, "PATCH")) {
-        response.status(200).send("success");
+        try {
+            const parsedFile: ParseResultType[] = JSON.parse(fileContents).data;
+            const dataToChange: ParsedFeedsDataType = request.body;
+            const { id, origin } = dataToChange;
+            const feedsSetRelatedToRequest = parsedFile.find(
+                (storedFeed: ParseResultType) =>
+                    storedFeed.originName === origin
+            );
+            if (feedsSetRelatedToRequest != null && feedsSetRelatedToRequest.feeds) {
+                const totalFeeds = [...feedsSetRelatedToRequest.feeds];
+                const oldFeed = totalFeeds.find((feed: ParsedFeedsDataType) => feed.id === id);
+                if (oldFeed != null) {
+                    const oldFeedIndex = totalFeeds.indexOf(oldFeed);
+                    totalFeeds[oldFeedIndex] = dataToChange;
+                }
+                const newData = {
+                    data: totalFeeds,
+                };
+                fs.writeFile(`${JSON_DIRECTORY}/feeds.json`, JSON.stringify(newData));
+            } else {
+                response.status(404).send('feed not found');
+            }
+            response.status(200).send("success");
+        } catch (error) {
+            response.status(400).send(error);
+        }
     } else if (areEqual(request.method, "DELETE")) {
         response.status(405).send("Method Not Allowed");
     }
