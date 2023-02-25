11f386af12520fad3ced05bd0bc47247c5154ab5
godcl1623 <godcl1623@gmail.com>
2023-02-26 03:34:25 +0900

    #25 feature: 쿠키 체크 및 사용자 아이디 반환 기능 구현

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,26 +1,77 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import { randomUUID } from 'crypto';
+import CryptoJS from "crypto-js";
+import { GetServerSidePropsContext } from 'next';
 
 export default class RequestControllers {
-  private instance: AxiosInstance;
+    private instance: AxiosInstance;
 
-  constructor(baseURL = process.env.NEXT_PUBLIC_REQUEST_URL) {
-    this.instance = axios.create({
-      baseURL,
-    });
-  }
+    constructor(baseURL = process.env.NEXT_PUBLIC_REQUEST_URL) {
+        this.instance = axios.create({
+            baseURL,
+        });
+    }
 
-  getDataFrom = (endpoint: string, option?: AxiosRequestConfig) =>
-    this.instance.get(endpoint, option);
+    getDataFrom = (endpoint: string, option?: AxiosRequestConfig) =>
+        this.instance.get(endpoint, option);
 
-  postDataTo = (endpoint: string, data: unknown, option?: AxiosRequestConfig) =>
-    this.instance.post(endpoint, data, option);
+    postDataTo = (
+        endpoint: string,
+        data: unknown,
+        option?: AxiosRequestConfig
+    ) => this.instance.post(endpoint, data, option);
 
-  putDataTo = (endpoint: string, data: unknown, option?: AxiosRequestConfig) =>
-    this.instance.put(endpoint, data, option);
+    putDataTo = (
+        endpoint: string,
+        data: unknown,
+        option?: AxiosRequestConfig
+    ) => this.instance.put(endpoint, data, option);
 
-  patchDataTo = (endpoint: string, data: unknown, option?: AxiosRequestConfig) =>
-    this.instance.patch(endpoint, data, option);
+    patchDataTo = (
+        endpoint: string,
+        data: unknown,
+        option?: AxiosRequestConfig
+    ) => this.instance.patch(endpoint, data, option);
 
-  deleteDataOf = (endpoint: string, option?: AxiosRequestConfig) =>
-    this.instance.delete(endpoint, option);
+    deleteDataOf = (endpoint: string, option?: AxiosRequestConfig) =>
+        this.instance.delete(endpoint, option);
 }
+
+export const getUserId = (context: GetServerSidePropsContext) => {
+    let userId = "";
+    if (
+        context.req.cookies.mw != null &&
+        typeof context.req.cookies.mw === "string"
+    ) {
+        userId = JSON.parse(decryptCookie(context.req.cookies.mw)).userId;
+    } else {
+        userId = randomUUID();
+    }
+    return userId;
+};
+
+export const checkIfCookieExists = (context: GetServerSidePropsContext) => {
+    if (
+        context.req.cookies.mw != null &&
+        typeof context.req.cookies.mw === "string"
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const encryptCookie = (target: { userId: string }) => {
+    return CryptoJS.AES.encrypt(
+        JSON.stringify(target),
+        process.env.MAGIC_WORD as string
+    ).toString();
+};
+
+export const decryptCookie = (target: string) => {
+    const decryptedTarget = CryptoJS.AES.decrypt(
+        target,
+        process.env.MAGIC_WORD as string
+    );
+    return decryptedTarget.toString(CryptoJS.enc.Utf8);
+};
