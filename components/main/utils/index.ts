a70685d0ad404ac469141377cb532abb7e783722
godcl1623 <godcl1623@gmail.com>
2023-04-16 01:22:00 +0900

    #23 fix: 페이지 표시기가 전체 페이지가 9쪽이 되지 않는 경우에도 9쪽까지 표시하는 문제 수정, 페이지 표시 조건문 switch문으로 수정

--- components/main/utils/index.ts
+++ components/main/utils/index.ts
@@ -1,7 +1,15 @@
-export const calculateTotalPages = (totalCount: number) => Math.ceil(totalCount / 10);
+export const calculateTotalPages = (totalCount: number) =>
+    Math.ceil(totalCount / 10);
 
 export const calculatePagesList = (currentPage: number, totalPages: number) => {
-    if (totalPages < 10 || currentPage <= 5) return Array.from({ length: 9 }, (_, page) => page + 1);
-    else if (currentPage >= totalPages - 4) return Array.from({ length: 9 }, (_, page) => totalPages + page - 8);
-    else return Array.from({ length: 9 }, (_, page) => currentPage + page - 4);
+    switch(true) {
+        case totalPages < 10 && currentPage <= 5:
+            return Array.from({ length: totalPages }, (_, page) => page + 1);
+        case totalPages >= 10 && currentPage <= 5:
+            return Array.from({ length: 9 }, (_, page) => page + 1);
+        case currentPage >= totalPages - 4:
+            return Array.from({ length: 9 }, (_, page) => totalPages + page - 8);
+        default:
+            return Array.from({ length: 9 }, (_, page) => currentPage + page - 4);
+    };
 };
