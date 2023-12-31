7a0f41fed1e5944772c5398f76144c530c1129cc
godcl1623 <godcl1623@gmail.com>
2024-01-01 01:25:25 +0900

    #40 feat: 개별 피드 상태 관리 호환성 수정

--- app/api/feeds/[feedsSetId]/[feedId]/route.ts
+++ app/api/feeds/[feedsSetId]/[feedId]/route.ts
@@ -1,17 +1,81 @@
-import { initializeMongoDBWith, newExtractUserIdFrom } from 'controllers/common';
-import { NextRequest, NextResponse } from 'next/server';
+import { ParseResultType, ParsedFeedsDataType } from "app/main";
+import {
+    initializeMongoDBWith,
+    newExtractUserIdFrom,
+} from "controllers/common";
+import {
+    copyArray,
+    findFeedToChange,
+    findRequestedFeedSetIndex,
+} from "controllers/feeds/feedsSetId/feedId";
+import { extractStoredFeedsFromRemote } from "controllers/feeds/new";
+import { parseCookie } from "controllers/utils";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(req: NextRequest) {
     try {
         const userId = newExtractUserIdFrom(req);
         if (userId == null) return NextResponse.error();
+        const rawId = parseCookie(userId);
         const { remoteData, Schema: Feeds } = await initializeMongoDBWith(
-            userId,
+            rawId,
             "feeds"
         );
-        return NextResponse.json({ userId });
+        const dataToChange: ParsedFeedsDataType = await req.json();
+        const { id, origin } = dataToChange;
+
+        const storedFeeds = extractStoredFeedsFromRemote(remoteData);
+        const feedsSetRelatedToRequest = storedFeeds.find(
+            (storedFeed: ParseResultType) => storedFeed.originName === origin
+        );
+
+        if (feedsSetRelatedToRequest?.feeds) {
+            const feedSetIndex = findRequestedFeedSetIndex(
+                storedFeeds,
+                feedsSetRelatedToRequest
+            );
+
+            const totalFeeds = copyArray(feedsSetRelatedToRequest.feeds);
+
+            const oldFeed = findFeedToChange(totalFeeds, id);
+            if (oldFeed != null) {
+                const oldFeedIndex = totalFeeds.indexOf(oldFeed);
+                totalFeeds[oldFeedIndex] = dataToChange;
+            }
+
+            const newFeedsSetRelatedToRequest = {
+                ...feedsSetRelatedToRequest,
+                feeds: totalFeeds,
+            };
+
+            storedFeeds[feedSetIndex] = newFeedsSetRelatedToRequest;
+            const updateResult = await Feeds?.updateOne(
+                { _uuid: rawId },
+                { $set: { data: storedFeeds } }
+            );
+            if (updateResult?.acknowledged) {
+                return NextResponse.json(
+                    JSON.stringify({ status: 200, message: "success" }),
+                    { status: 200, statusText: "success" }
+                );
+            } else {
+                return NextResponse.json(
+                    JSON.stringify({ status: 400, message: "update failed" }),
+                    { status: 400, statusText: "update failed" }
+                );
+            }
+        } else {
+            return NextResponse.json(
+                JSON.stringify({ status: 404, message: "feed not found" }),
+                { status: 404, statusText: "feed not found" }
+            );
+        }
     } catch (error) {
-        return NextResponse.error();
+        console.error(error);
+        return NextResponse.json(
+            JSON.stringify({ status: 400, message: "update failed" }),
+            { status: 400, statusText: "update failed" }
+        );
     }
 }
 
