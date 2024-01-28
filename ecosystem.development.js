9ab73ab5921082c5a57c77760a7e28146c4ed48a
godcl1623 <godcl1623@gmail.com>
2024-01-29 00:07:00 +0900

    #48 feat: pm2 development ecosystem파일에 인스턴스, 실행 모드 명시적으로 지정

--- ecosystem.development.js
+++ ecosystem.development.js
@@ -3,6 +3,8 @@ module.exports = {
         {
             name: "start-page",
             script: "build/server.js",
+            instances: "1",
+            exec_mode: "fork",
             autorestart: true,
             time: true,
             interpreter_args: ["--require=./.pnp.cjs"],
