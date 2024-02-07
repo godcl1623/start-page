79c9618b16ec9f01c8c864ce179ae478d7166cb5
godcl1623 <godcl1623@gmail.com>
2024-02-07 17:06:56 +0900

    #51 test: 디버그용 코드 추가

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,6 +2,7 @@ import { encryptCookie, getNewUserId } from 'controllers/utils';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
+    console.log('origin input: ', req.nextUrl.origin, ', origin set: ', process.env.REGISTER_ORIGIN)
     try {
         if (req.nextUrl.origin !== process.env.REGISTER_ORIGIN) {
             return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
