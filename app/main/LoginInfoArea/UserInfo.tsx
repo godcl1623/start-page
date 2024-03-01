ea1f5510a39623c257ea84aa838b7804e5dd37ca
godcl1623 <godcl1623@gmail.com>
2024-03-01 20:13:31 +0900

    #65 chore: 기존 useResizeEvent로 글자수를 조절하던 항목 중 css의 text-ellipsis로 대체 가능한 항목들에 대해 text-ellipsis 적용

--- app/main/LoginInfoArea/UserInfo.tsx
+++ app/main/LoginInfoArea/UserInfo.tsx
@@ -1,5 +1,4 @@
-import useResizeEvent from "hooks/useResizeEvent";
-import { forwardRef, useCallback, useEffect, useState } from "react";
+import { forwardRef } from "react";
 
 interface Props {
     userEmail: string | null | undefined;
@@ -10,35 +9,16 @@ export default forwardRef<HTMLButtonElement, Props>(function UserInfo(
     { userEmail, handleDataHandler }: Props,
     ref
 ) {
-    const [signedInUser, setSignedInUser] = useState(userEmail);
-
-    const adjustUserIdOnResize = useCallback(() => {
-        if (userEmail != null) {
-            if (document.documentElement.offsetWidth < 360) {
-                setSignedInUser(userEmail.slice(0, 8) + "...");
-            } else if (
-                document.documentElement.offsetWidth >= 360 &&
-                document.documentElement.offsetWidth < 768
-            ) {
-                setSignedInUser(userEmail.slice(0, 18) + "...");
-            } else {
-                setSignedInUser(userEmail);
-            }
-        }
-    }, [userEmail]);
-
-    useResizeEvent(adjustUserIdOnResize, true, [adjustUserIdOnResize]);
-
     return (
         <button
             type="button"
             ref={ref}
-            className="px-3 py-2 rounded-md rounded-r-none shadow-md bg-neutral-500 font-bold text-sm text-neutral-100 dark:shadow-zinc-600 whitespace-pre"
+            className="px-3 py-2 rounded-md rounded-r-none shadow-md bg-neutral-500 font-bold text-sm text-neutral-100 dark:shadow-zinc-600 whitespace-nowrap overflow-hidden text-ellipsis"
             onClick={() => {
                 if (handleDataHandler != null) handleDataHandler();
             }}
         >
-            {signedInUser ?? "Guest"}
+            {userEmail ?? "Guest"}
         </button>
     );
 });
